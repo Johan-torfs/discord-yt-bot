819bf7c09f73d3d06df0a7c7c6b94bd7ba90f9f2
Johan Torfs <j.t.johan.torfs@gmail.com>
2023-11-07 04:12:28 +0100

    try hosting on vercel with serverless functions

--- src/index.js
+++ src/index.js
@@ -8,25 +8,23 @@ const TOKEN = process.env.DISCORD_TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
 const CLIENT_ID = process.env.CLIENT_ID;
 
-export default function startBot() {
-    // Create REST and WebSocket managers directly
-    const rest = new REST({ version: '10' }).setToken(TOKEN);
+// Create REST and WebSocket managers directly
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
-    // Create a client to emit relevant events.
-    const client = new Client({ 
-        intents: [
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.GuildVoiceStates,
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.MessageContent,
-        ]
-    });
+// Create a client to emit relevant events.
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+    ]
+});
 
-    // Listen for the ready event
-    client.once('ready', () => console.log('Ready!'));
+// Listen for the ready event
+client.once('ready', () => console.log('Ready!'));
 
-    registerApplicationCommands(rest, CLIENT_ID, GUILD_ID);
-    startInteractionListener(client);
+registerApplicationCommands(rest, CLIENT_ID, GUILD_ID);
+startInteractionListener(client);
 
-    client.login(TOKEN);
-}
+client.login(TOKEN);
